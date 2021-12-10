f49209af40c1033078d593be5b67ed0125564208
Pantelis Papasavvas <pantpap@gmail.com>
2021-12-10 15:16:08 +0200

    feat(api service): add method on fetch

--- services/apiService.js
+++ services/apiService.js
@@ -1,7 +1,9 @@
 import GET_USER from "../api";
 
+const headers = new Headers({'Accept': 'application/vnd.github.v3+json'});
+
 export const getUserInfo = (user) => {
     let username = user.toLowerCase().trim();
     const URL = `${GET_USER}/${username}`;
-    return fetch(URL).then((res) => res.json);
+    return fetch(URL, {headers, method:'GET'}).then((res) => res.json);
 }
