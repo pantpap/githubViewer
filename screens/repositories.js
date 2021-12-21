ef3cf73a51c53ff28bd058d8df8b349717cf8274
Pantelis Papasavvas <pantpap@gmail.com>
2021-12-21 16:46:09 +0200

    feat:(repositories) repos component impl

--- screens/repositories.js
+++ screens/repositories.js
@@ -1,13 +1,31 @@
-import React from 'react';
-import { View, StyleSheet, TextInput, Text } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, StyleSheet, TextInput, Text, ToastAndroid} from 'react-native';
 
+const Repositories = ({route, navigation}) => {
+  const userData = route?.params?.userData;
+  const {repos_url} = userData;
 
+  const [data, setData] = useState('');
 
+  const getUserRepos = async () => {
+    console.log('repos', userData);
+    try {
+      const res = await fetch(repos_url);
+      const response = await res.json();
+      setData (() => response);
+      console.log(response)
+    }catch (e) {
+      ToastAndroid.show(error, ToastAndroid.SHORT);
+    }
+  }
 
-const Repositories = ({navigation}) => {
+  useEffect(() => {
+    getUserRepos();
+  }, []);
+
   return (
   <View style={styles.container}>
-    <Text>Repositories</Text>
+    <Text>{data.name}</Text>
    
   </View>
 )};
