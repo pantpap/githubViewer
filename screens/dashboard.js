43e2e7e970f28a87f368422026cc8968ff6bafeb
Pantelis Papasavvas <pantpap@gmail.com>
2021-12-21 16:45:12 +0200

    feat:(dashboard) configure dashboard component

--- screens/dashboard.js
+++ screens/dashboard.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
-
+import {View, StyleSheet, Text, Image, ScrollView, StatusBar, FlatList} from 'react-native';
 import {PROFILE, REPOSITORIES, FOLLOWERS} from "../navigation";
 
 
 const Dashboard = ({route, navigation}) => {
   const userData = route?.params?.json;
-
-  const { name } = userData;
+  const { avatar_url } = userData;
 
   const navigate = (page) =>{
-    navigation.navigate(page)
+    navigation.navigate(page, {userData})
   }
 
   return (
   <View style={styles.container}>
-    <Text>{name}</Text>
+    <Image
+        style={styles.avatar}
+        source={{uri: avatar_url}}
+    />
     <Text style={[styles.buttons, {backgroundColor: '#5cd9ff'}]} onPress={()=>navigate(PROFILE)}>View Profile</Text>
     <Text style={[styles.buttons, {backgroundColor: '#ff6060'}]} onPress={()=>navigate(REPOSITORIES)}>View Repos</Text>
     <Text style={[styles.buttons, {backgroundColor: '#7579f3'}]} onPress={()=>navigate(FOLLOWERS)}>View Followers</Text>
 
-  </View>
+  </View >
   )
 };
 
@@ -28,8 +29,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
-    justifyContent: 'center',
-    alignItems: 'center'
+    paddingTop: StatusBar.currentHeight
   },
   buttons: {
     width: '100%',
@@ -37,8 +37,14 @@ const styles = StyleSheet.create({
     padding: 15,
     fontSize: 22,
     marginBottom: 10,
-    textAlign: 'center'
-  }
+    textAlign: 'center',
+  },
+  avatar: {
+    height: 300,
+    marginBottom: 5,
+    borderRadius: 4,
+    resizeMode: 'cover',
+  },
 });
 
 export default Dashboard;
