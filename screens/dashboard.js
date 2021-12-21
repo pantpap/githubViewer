26e39ae1169674b3100016a637425429a5f9b2ad
Pantelis Papasavvas <pantpap@gmail.com>
2021-12-21 11:21:23 +0200

    feat:(dashboard) navigation buttons routing and style

--- screens/dashboard.js
+++ screens/dashboard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 
-
+import {PROFILE, REPOSITORIES, FOLLOWERS} from "../navigation";
 
 
 const Dashboard = ({route, navigation}) => {
@@ -9,10 +9,17 @@ const Dashboard = ({route, navigation}) => {
 
   const { name } = userData;
 
+  const navigate = (page) =>{
+    navigation.navigate(page)
+  }
+
   return (
   <View style={styles.container}>
     <Text>{name}</Text>
-   
+    <Text style={[styles.buttons, {backgroundColor: '#5cd9ff'}]} onPress={()=>navigate(PROFILE)}>View Profile</Text>
+    <Text style={[styles.buttons, {backgroundColor: '#ff6060'}]} onPress={()=>navigate(REPOSITORIES)}>View Repos</Text>
+    <Text style={[styles.buttons, {backgroundColor: '#7579f3'}]} onPress={()=>navigate(FOLLOWERS)}>View Followers</Text>
+
   </View>
   )
 };
@@ -23,6 +30,14 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  buttons: {
+    width: '100%',
+    color: '#fff',
+    padding: 15,
+    fontSize: 22,
+    marginBottom: 10,
+    textAlign: 'center'
   }
 });
 
