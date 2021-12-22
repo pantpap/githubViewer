96e1756c9216c4c45b90cd069f0e3b56d1ab87ab
Pantelis Papasavvas <pantpap@gmail.com>
2021-12-22 12:17:42 +0200

    display repos on reposList

--- components/ReposList.js
+++ components/ReposList.js
@@ -2,11 +2,34 @@ import React from "react";
 import {Text, View} from "react-native";
 
 const ReposList = (props) => {
+    const {repos} = props;
+
     return (
-        <View style={style.view}>
-            <Text>{props.name}</Text>
+        <View style={{
+            borderBottomWidth: 1,
+            borderBottomColor: '#ccc',
+            padding: 8
+        }}>
+            {/*<View style={{flexDirection:"row"}}>*/}
+                <Text style={[styles.common, styles.repoName]}>{repos.name}</Text>
+            {/*</View>*/}
+            {/*<View style={{flexDirection:"row"}}>*/}
+                <Text style={[styles.common]}>Stars: {repos.stargazers_count}</Text>
+            {/*</View>*/}
+            {/*<View style={{flexDirection:"row"}}>*/}
+                <Text>{repos.description}</Text>
+            {/*</View>*/}
         </View>
     )
+}
+
+const styles = {
+    common: {
+        color: 'skyblue'
+    },
+    repoName: {
+        fontSize: 22,
+    }
 }
 
 export default ReposList;
